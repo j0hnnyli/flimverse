7873799b16577fcd65abc04e6a58a16f8e21eeb9
Johnny li <johnnyli@Johnnys-Laptop.local>
2024-08-16 20:41:56 -0400

    modulized routes

---
+++ server/routes/anime_route.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, jsonify
+import requests
+
+anime_bp = Blueprint('anime', __name__)
+
+JIKAN_BASE_URL = 'https://api.jikan.moe/v4'
+
+@anime_bp.route('/anime/populars')
+def get_animes():
+    try:
+        response = requests.get(f'{JIKAN_BASE_URL}/top/anime')  # Adjust endpoint if needed
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+        data = response.json()
+        return jsonify({
+            'message': 'List of animes',
+            'data': data['data']  # Adjust based on actual API response structure
+        })
+    except requests.RequestException as e:
+        return jsonify({'error': str(e)}), 500
+    
+@anime_bp.route('/anime/<int:id>')
+def get_anime(id):
+    try:
+        response = requests.get(f'{JIKAN_BASE_URL}/anime/{id}')
+        response.raise_for_status()
+        data = response.json()
+        return jsonify({
+            'message' : f'Anime {id}',
+            'data' : data
+        })
+    except requests.RequestException as e:
+        return jsonify({'error': str(e)})
+    
