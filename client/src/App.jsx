d5ba29bb81c35a4bfa096284cd4f824418b65475
Allen <allen.kong21@gmail.com>
2024-08-16 20:32:54 -0400

    Added anime page, tv page, movies page, and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './components/Home'
+import AnimePage from './components/AnimePage'
+import TVPage from './components/TVPage'
+import MoviesPage from './components/MoviesPage'
 
 function App() {
 
@@ -8,6 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={ <Home/> }/>
+          <Route path='/anime' element={ <AnimePage/> }/>
+          <Route path='/tv' element={ <TVPage/> }/>
+          <Route path='/movies' element={ <MoviesPage/> }/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/components/AnimePage.jsx
@@ -0,0 +1,9 @@
+const AnimePage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-center">Hello World of Anime</h1>
+    </div>
+  );
+};
+
+export default AnimePage;
