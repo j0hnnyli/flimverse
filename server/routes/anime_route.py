65ebcd4b45f597c3fbf167ce7ea8297bcdfe684c
Johnny li <johnnyli@Johnnys-Laptop.local>
2024-08-18 00:34:53 -0400

    search routes

--- server/routes/anime_route.py
+++ server/routes/anime_route.py
@@ -5,10 +5,10 @@ anime_bp = Blueprint('anime', __name__)
 
 JIKAN_BASE_URL = 'https://api.jikan.moe/v4'
 
-@anime_bp.route('/animes/populars')
-def get_animes():
+@anime_bp.route('/animes/populars/<int:page>')
+def get_animes(page):
     try:
-        response = requests.get(f'{JIKAN_BASE_URL}/top/anime')  # Adjust endpoint if needed
+        response = requests.get(f'{JIKAN_BASE_URL}/top/anime?page={page}')  # Adjust endpoint if needed
         response.raise_for_status()  # Raise an HTTPError for bad responses
         data = response.json()
         return jsonify({
@@ -30,4 +30,17 @@ def get_anime(id):
         })
     except requests.RequestException as e:
         return jsonify({'error': str(e)})
-    
+
+@anime_bp.route('/animes/<string:value>/<int:page>')
+def get_animes_search(value, page):
+    try:
+        url = f'{JIKAN_BASE_URL}/anime?q={value}?page={page}'
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return jsonify({
+            'message' : f'Animes - {value}',
+            'data' : data
+        })
+    except requests.RequestException as e:
+        return jsonify({'error' : str(e)})   
--- server/routes/tvshows_route.py
+++ server/routes/tvshows_route.py
@@ -4,16 +4,16 @@ import os
 
 TMDB_API_KEY = os.getenv('TMDB_API_READ_ACCESS')
 tvshows_bp = Blueprint('tvshows', __name__)
-BASE_URL = 'https://api.themoviedb.org/3/tv'
+BASE_URL = 'https://api.themoviedb.org/3/'
 headers = {
     "accept": "application/json",
     "Authorization": f"Bearer {TMDB_API_KEY}"
 }
 
-@tvshows_bp.route('/tvshows/populars')
-def get_tvshows():
+@tvshows_bp.route('/tvshows/populars/<int:page>')
+def get_tvshows(page):
     try:
-        url = f"{BASE_URL}/popular?language=en-US&page=1"
+        url = f"{BASE_URL}tv/popular?language=en-US&page={page}"
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         data = response.json()
@@ -27,12 +27,26 @@ def get_tvshows():
 @tvshows_bp.route('/tvshows/<int:id>')
 def get_tvshow(id):
     try:
-        url = f'{BASE_URL}/{id}'
+        url = f'{BASE_URL}tv/{id}'
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         data = response.json()
         return jsonify({
             'message' : f'Tv_Show {id}',
+            'data' : data
+        })
+    except requests.RequestException as e:
+        return jsonify({'error' : str(e)})
+
+@tvshows_bp.route('/tvshows/<string:value>/<int:page>')
+def get_tvshows_search(value, page):
+    try:
+        url = f'{BASE_URL}search/tv?query={value}&language=en-US&page={page}'
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return jsonify({
+            'message' : f'TvShows - {value}',
             'data' : data
         })
     except requests.RequestException as e:
