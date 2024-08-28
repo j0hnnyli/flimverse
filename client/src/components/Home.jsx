e8ab35129b8b66e5879457ac2798df50cb6efef1
Johnny li <johnnyli@Johnnys-Laptop.local>
2024-08-28 13:07:39 -0400

    new nav

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -47,23 +47,19 @@ const Home = () => {
         <h1>FlimVerse</h1>
       </div>
 
-      <div className="absolute top-10 left-10 text-orange-400 text-4xl tracking-wide">
-        {slides[index].name}
-      </div>
-
-      <div className="absolute bottom-10 left-10 z-20">
+      <div className="absolute top-10 left-10 z-20">
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuTrigger className="text-xl text-orange-400 cursor-pointer">
-                Explore Categories
+              <NavigationMenuTrigger className="text-xl bg-orange-400 text-white">
+                {slides[index].name}
               </NavigationMenuTrigger>
               <NavigationMenuContent className="p-4 bg-gray-800 text-white rounded-lg">
                 {slides.map((slide, i) => (
                   <Link
                     to={slide.link}
                     key={i}
-                    className={`flex items-center gap-3 p-3 bg-gray-700 rounded-lg hover:bg-gray-600 cursor-pointer mb-2`}
+                    className='flex items-center gap-3 p-3 bg-gray-700 rounded-lg hover:bg-gray-600 cursor-pointer mb-2'
                     onClick={() => setIndex(i)}
                   >
                     <div className={`w-[50px] h-[50px] bg-center bg-cover ${index === i ? 'border-orange-400 border-2' : ''}`} style={{ backgroundImage: `url(${slide.url})` }}></div>
